dfd39003eeb3412eeee7ef680a3249e5527855bf
ibraheemovicjnr4 <ibraheemariyo4190@gmail.com>
2025-05-23 14:39:52 +0100

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "DML",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "DML",              
+    OWNER_NAME: process.env.OWNER_NAME || "Ibraheemovicjnr",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "2349137139054",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'yes',
     BOT : process.env.BOT_NAME || 'DML-MD',
